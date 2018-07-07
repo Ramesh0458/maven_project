d3cf95becfa2e13f97e5ed3698b62943d9fc5b06
shekhar <boreddyshekhar@gmail.com>
2018-07-07 12:48:22 +0530

    maven_project

---
+++ src/main/java/paytm/paytm.java
@@ -0,0 +1,18 @@
+package paytm;
+
+public class paytm
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello world!" );
+
+
+        System.out.println( "Hello world!" );
+
+        System.out.println( "Hello java world!" );
+		
+    }
+
+
+}
